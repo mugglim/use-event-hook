d51113cae835fa973102c9cb54325d44e477c118
jaeuk lim <applewodnr@naver.com>
2022-11-06 14:58:11 +0900

    refactor: 페이지 마다 색상이 바뀌도록 변경

--- my-app/src/components/Expensive.tsx
+++ my-app/src/components/Expensive.tsx
@@ -1,7 +1,5 @@
 import { getColor } from '@utils';
 
-const DUMMY_LIST = Array.from(new Array(5_000), (_, i) => [i, getColor()]);
-
 const Expensive = ({ onClick }: ExpensiveProps) => {
   return (
     <div
@@ -13,7 +11,7 @@ const Expensive = ({ onClick }: ExpensiveProps) => {
         marginTop: 20,
       }}
     >
-      {DUMMY_LIST.map(([value, color], i) => (
+      {getDummyList().map(([value, color], i) => (
         <div
           key={`${i}-${value}`}
           style={{ width: 10, height: 10, backgroundColor: `${color}` }}
@@ -21,6 +19,10 @@ const Expensive = ({ onClick }: ExpensiveProps) => {
       ))}
     </div>
   );
+};
+
+const getDummyList = () => {
+  return Array.from(new Array(5_000), (_, i) => [i, getColor()]);
 };
 
 export interface ExpensiveProps {
