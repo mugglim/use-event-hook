72682808047682c8f95601f0f2b13c86d0d7aa37
jaeuk lim <applewodnr@naver.com>
2022-11-06 16:21:06 +0900

    feat: 메인 페이지에도 헤더 링크 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Header from '@components/Header';
+
 const Home = () => {
-  return <>Hello </>;
+  return (
+    <>
+      <h2>Hello. This is useEvent hook playground</h2>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
